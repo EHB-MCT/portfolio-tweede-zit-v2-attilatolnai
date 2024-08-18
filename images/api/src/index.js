fd459141eb449b93d000dd5f22eaf0d6a1fd1815
attilatolnai <attila.tolnai@student.ehb.be>
2024-08-18 17:23:57 +0200

    feat: add body-parser and GET and POST requests
    Installed body-parser and added a new GET and a new POST. The GET and POST have been tested in Postman and they work

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -1,16 +1,51 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+const port = 3000;
 
-app.get("/", (request, response) => {
-    response.send({ message: "hello world"})
+app.use(bodyParser.json());
+
+let posts = [];
+
+app.get("/", (req, res) => {
+    res.send({ message: "hello world"})
 })
 
-app.listen(3000, (err) => {
+app.get("/posts", (req, res) =>{
+    res.json(posts);
+});
+
+app.post("/posts", (req, res) => {
+    const { title, content } = req.body;
+    if (!title || !content) {
+        return res.status(400).json({error: "Title and content are required" });
+    }
+
+    const newPost = {
+        id: posts.length +1,
+        title,
+        content,
+        createdAt: new Date()
+    };
+    posts.push(newPost);
+    res.status(201).json(newPost);
+});
+
+app.get("/posts/:id", (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+    const post = posts.find(p => p.id === postId);
+    if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+    }
+    res.json(post);
+});
+
+app.listen(port, (err) => {
     if(!err) {
-        console.log("running on port "  + 3000);
+        console.log(`server running on port ${port}`);
     }
     else{
         console.error(err)
     }
-})
+});
